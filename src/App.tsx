426ee1ad56745ed05df8e117c5e7933959dcde1d
Konixy <konixy.p@gmail.com>
2022-12-27 18:30:48 +0100

    Update

--- src/App.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-function App() {
-  return <div className="text-center text-xl font-semibold">app</div>;
-}
-
-export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Canvas } from '@react-three/fiber';
 import './index.scss';
-import App from './App';
+import { Scene } from './Scene';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Canvas>
+    <Scene />
+  </Canvas>,
 );
